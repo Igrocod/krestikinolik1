6087dbaa07013324e84dea365355785a8080d827
Igrocod <69511043+Igrocod@users.noreply.github.com>
2021-10-27 16:56:14 +0300

    lox

---
+++ лох.py
@@ -0,0 +1,12 @@
+t=False
+while (t==False):
+    a=input('X or 0')
+    if (a=='X' or a=='0'):
+        if a=='X':
+            print('X for downs')
+            t=True
+        if a=='0':
+            print('o for downs')
+            t=True
+        else:
+            print(
